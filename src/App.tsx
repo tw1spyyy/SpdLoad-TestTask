a9734047bfb5629a831245641ba7efb8d32af287
roman <grstroma.it@gmail.com>
2023-06-23 12:57:50 +0300

    preparing project to develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => {
+	return (
+		<div>
+			<Routes>
+				<Route path="/home" element />
+				<Route path="/favorites" element />
+				<Route path="*" element={<Navigate to="/home" />} />
+			</Routes>
+		</div>
+	);
+};
